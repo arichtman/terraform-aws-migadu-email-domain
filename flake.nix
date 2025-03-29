8f602a4b81da64d9d5ded8920c96fe220510363b
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2025-03-29 14:59:35 +1000

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'flake-utils':
        'github:numtide/flake-utils/a1720a10a6cfe8234c0e93907ffe81be440f4cef?narHash=sha256-o2d0KcvaXzTrPRIo0kOLV0/QXHhDQ5DTi%2BOxcjO8xqY%3D' (2023-05-31)
      → 'github:numtide/flake-utils/11707dc2f618dd54ca8739b309ec4fc024de578b?narHash=sha256-l0KFg5HjrsfsO/JpG%2Br7fRrqm12kzFHyUHqHCVpMMbI%3D' (2024-11-13)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/af21c31b2a1ec5d361ed8050edd0303c31306397?narHash=sha256-LTNlJcru2qJ0XhlhG9Acp5KyjB774Pza3tRH0pKIb3o%3D' (2023-06-06)
      → 'github:NixOS/nixpkgs/6c5963357f3c1c840201eda129a99d455074db04?narHash=sha256-yQugdVfi316qUfqzN8JMaA2vixl%2B45GxNm4oUfXlbgw%3D' (2025-03-27)
    • Updated input 'poetry2nix':
        'github:nix-community/poetry2nix/b4d113ba3e710b1408a11234e10f1dfd9416b2dc?narHash=sha256-K0SSXih5mP7M9kH/rln6LhWdMbmGs9LZcjx2jLnmK/Q%3D' (2023-06-06)
      → 'github:nix-community/poetry2nix/b9a98080beff0903a5e5fe431f42cde1e3e50d6b?narHash=sha256-nzgO/ZCSBzWjbMkYDxG%2Byl9Z2eGbCgQu06Oku3ir5D4%3D' (2025-03-19)
    • Updated input 'poetry2nix/flake-utils':
        'github:numtide/flake-utils/a1720a10a6cfe8234c0e93907ffe81be440f4cef?narHash=sha256-o2d0KcvaXzTrPRIo0kOLV0/QXHhDQ5DTi%2BOxcjO8xqY%3D' (2023-05-31)
      → 'github:numtide/flake-utils/c1dfcf08411b08f6b8615f7d8971a2bfa81d5e8a?narHash=sha256-X6rJYSESBVr3hBoH0WbKE5KvhPU5bloyZ2L4K60/fPQ%3D' (2024-09-17)
    • Added input 'poetry2nix/nix-github-actions':
        'github:nix-community/nix-github-actions/e04df33f62cdcf93d73e9a04142464753a16db67?narHash=sha256-B4mzTcQ0FZHdpeWcpDYPERtyjJd/NIuaQ9%2BBV1h%2BMpA%3D' (2024-10-24)
    • Added input 'poetry2nix/nix-github-actions/nixpkgs':
        follows 'poetry2nix/nixpkgs'
    • Added input 'poetry2nix/systems':
        'github:nix-systems/default/da67096a3b9bf56a91d16901293e51ba5b49a27e?narHash=sha256-Vy1rq5AaRuLzOxct8nz4T6wlgyUR7zLU309k9mBC768%3D' (2023-04-09)
    • Added input 'poetry2nix/treefmt-nix':
        'github:numtide/treefmt-nix/9ef337e492a5555d8e17a51c911ff1f02635be15?narHash=sha256-LqHYIxMrl/1p3/kvm2ir925tZ8DkI0KA10djk8wecSk%3D' (2024-10-28)
    • Added input 'poetry2nix/treefmt-nix/nixpkgs':
        follows 'poetry2nix/nixpkgs'

--- flake.nix
+++ flake.nix
@@ -2,47 +2,105 @@
 {
   description = "IaC using Nix";
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
   outputs = {
     self,
     nixpkgs,
-    poetry2nix,
     flake-utils,
+    uv2nix,
+    pyproject-nix,
+    pyproject-build-systems,
     ...
   } @ inputs:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ]
     (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+    let
+    # Ref: https://github.com/FerranAD/simple-uv2nix
+      inherit (nixpkgs) lib;
+
+      # Load a uv workspace from a workspace root.
+      # Uv2nix treats all uv projects as workspace projects.
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+      # Use Python 3.12 from nixpkgs
+      python = pkgs.python312;
+
+    	pkgs = import nixpkgs {
+    	  inherit system;
+    	  config.allowUnfree = true;
+    	};
+
+      overlay = workspace.mkPyprojectOverlay {
+        sourcePreference = "wheel";
+      };
+      baseSet = pkgs.callPackage inputs.pyproject-nix.build.packages {
+        inherit python;
+      };
+      pythonSet = baseSet.overrideScope (
+        pkgs.lib.composeManyExtensions [
+          inputs.pyproject-build-systems.overlays.default
+          overlay
+        ]
+      );
+      venv = pythonSet.mkVirtualEnv "venv" workspace.deps.all;
+    in
+    {
+      devShells = {
+        default = pkgs.mkShell {
+          packages = [
+            python
+            venv
+            pkgs.uv
+            pkgs.terraform
+            pkgs.terraform-docs
+            pkgs.alejandra
+            pkgs.checkov
+            pkgs.terraform-ls
+          ];
+          env =
+            {
+              # Don't create venv using uv
+              UV_NO_SYNC = "1";
+
+              # Prevent uv from managing Python downloads
+              UV_PYTHON_DOWNLOADS = "never";
+              # Force uv to use nixpkgs Python interpreter
+              # UV_PYTHON = python.interpreter;
+            }
+            // lib.optionalAttrs pkgs.stdenv.isLinux {
+              # Python libraries often load native shared objects using dlopen(3).
+              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+            };
+          shellHook = ''
+            unset PYTHONPATH
+            # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
+            export REPO_ROOT=$(git rev-parse --show-toplevel)
+            pre-commit install --install-hooks
+          '';
         };
-        #TODO: Why can't we access inputs.poetry2nix.mkPoetryEnv directly?
-        inherit (poetry2nix.legacyPackages.${system}) mkPoetryEnv;
-        poetryEnv = mkPoetryEnv {
-          projectDir = ./.;
-        };
-      in {
-        devShells.default = with pkgs;
-          mkShell {
-            nativeBuildInputs = [
-              poetryEnv
-              terraform
-              terraform-docs
-              poetry
-              alejandra
-              checkov
-              terraform-ls
-            ];
-            shellHook = ''
-              pre-commit install --install-hooks
-            '';
-          };
-      }
-    );
+
+      };
+    });
 }
